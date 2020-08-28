1c7e5e4030cb8f4f7b4de96fba5eba5f57b15446
Vulpie <wojtas_f@outlook.com>
2020-08-28 12:53:26 +0200

    feat: add basic files and install discord.js

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Discord bot
+
+Discord bot connected with rasa bot framework
---
+++ src/bot.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+
+const discod_bot_token = process.env.DISCORDJS_BOT_TOKEN
+console.log(discod_bot_token)
